095650772936387c8de03520e351d8b16127f1be
VitaPetrushyna <masandra2018@ukr.net>
2022-07-27 20:54:57 +0300

    Создает разметку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const divGalleryEl = document.querySelector(".gallery");
+// console.log(divGalleryEl);
+
+const cardsMarcup = createItemsGalleryMarcup(galleryItems);
+// console.log(cardsMarcup);
+
+divGalleryEl.insertAdjacentHTML("beforeend", cardsMarcup);
+
+divGalleryEl.addEventListener("click", onOpenItem);
+
+function createItemsGalleryMarcup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `;
+    })
+    .join("");
+  //   console.log(marcup);
+}
+
+function onOpenItem(evt) {
+  evt.preventDefault();
+  console.log(evt.target);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGalleryEl = document.querySelector(".gallery");
+const cardsMarcup = createItemsGalleryMarcup(galleryItems);
+
+divGalleryEl.insertAdjacentHTML("beforeend", cardsMarcup);
+
+function createItemsGalleryMarcup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join("");
+}
